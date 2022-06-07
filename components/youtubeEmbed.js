d4333e6b376b43dd86d61e09791c7c176cce4189
b4ljk <baljinnyam.dayan@gmail.com>
2022-06-07 16:49:09 +0800

    youtube autoplay is in another levle

--- components/youtubeEmbed.js
+++ components/youtubeEmbed.js
@@ -1,15 +1,19 @@
-const YoutubeEmbed = ({ id }) => (
-  <div className="video-responsive">
-    <iframe
-      width="100%"
-      className="h-[100vh]"
-      src={`https://www.youtube.com/embed/${id}?autoplay=1&mute=1`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; autoplay"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
+import { useState } from "react";
+const YoutubeEmbed = ({ id }) => {
+  const [isPlaying, setIsPlaying] = useState(0);
+  return (
+    <div className="video-responsive">
+      <iframe
+        width="100%"
+        className="h-[100vh]"
+        src={`https://www.youtube.com/embed/${id}?autoplay=${isPlaying}&mute=1`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; autoplay"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
+};
 
 export default YoutubeEmbed;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Hero from "../components/hero";
 import Feature from "../components/feature";
-import YoutubeEmbed from "../components/youtubeEmbed";
+// import YoutubeEmbed from "../components/youtubeEmbed";
 import Feature2 from "../components/feature2";
 import Feature3 from "../components/feature3";
-
+import MySectionBlock from "../components/viewport";
 export default function Home() {
   return (
     <>
       <Hero />
       <Feature />
-      <YoutubeEmbed id="gIx2F0o2jWQ" />
+      <MySectionBlock playerId="gIx2F0o2jWQ" />
       <Feature2 />
       <Feature3 />
     </>
