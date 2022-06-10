a8962b6c1465d835ce31f23a00b004a76dc481d0
tergel <tergel@tanasoft.mn>
2022-06-10 19:41:12 +0800

    bullet color change added

--- components/feature4.js
+++ components/feature4.js
@@ -1,5 +1,39 @@
-import { Box } from "@chakra-ui/react";
+import { Box, chakra, SimpleGrid, Image } from "@chakra-ui/react";
+
+import feature4Img from "../assets/img/feature4.png";
 
 export default function Feature4() {
-  return <Box>test</Box>;
+  return (
+    <Box h="100vh" pl={{ base: 4, md: 12, "2xl": "15rem" }} mx="auto">
+      <SimpleGrid alignItems="start" columns={{ base: 1, md: 2 }}>
+        <Box my="auto">
+          <chakra.h2
+            mb={4}
+            fontFamily="SFProRounded"
+            fontSize={{ base: "2xl", md: "55px" }}
+            lineHeight="normal"
+            w="550px"
+            textColor="#fa6c51"
+            fontWeight={300}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            Яг одоо аппликейшнээ татаарай
+          </chakra.h2>
+          <chakra.p
+            mb={5}
+            textAlign={{ base: "center", sm: "left" }}
+            fontFamily="SFProRounded"
+            fontWeight={300}
+            lineHeight="1.39"
+            fontSize={{ md: "18px" }}
+          >
+            asdasd
+          </chakra.p>
+        </Box>
+        <Box display="flex" w="full" h="full">
+          <Image mt="auto" w="full" src={feature4Img?.src} />
+        </Box>
+      </SimpleGrid>
+    </Box>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import Hero from "../components/hero";
 import Feature2 from "../components/feature2";
 import Feature3 from "../components/feature3";
+import Feature4 from "../components/feature4";
 import YoutubeEmbed from "../components/viewport";
 
 const FeatureWithNoSSR = dynamic(() => import("../components/feature"), { ssr: false });
@@ -15,6 +16,7 @@ export default function Home() {
       <YoutubeEmbed playerId="gIx2F0o2jWQ" />
       <Feature2 />
       <Feature3 />
+      <Feature4 />
     </>
   );
 }
