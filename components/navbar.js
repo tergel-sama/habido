62ce9adc2db9110913027ca3535f11242646e11f
tergel <tergel@tanasoft.mn>
2022-06-23 15:09:20 +0800

    navbar fix 3/3

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,4 @@
-import {
-  Box,
-  Flex,
-  Collapse,
-  Text,
-  IconButton,
-  Button,
-  Stack,
-  useBreakpointValue,
-  useDisclosure,
-  Image,
-} from "@chakra-ui/react";
+import { Box, Flex, Collapse, Text, IconButton, Button, Stack, useDisclosure, Image } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 import LogoImage from "../assets/img/navbarLogo.png";
@@ -28,13 +17,19 @@ export default function WithSubnavigation() {
             aria-label="Toggle Navigation"
           />
         </Flex>
-        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
-          <Text textAlign={useBreakpointValue({ base: "center", md: "left" })} fontFamily="heading">
+        <Flex flex={{ base: 1 }} justify={{ base: "end", md: "start" }}>
+          <Text fontFamily="heading">
             <Image src={LogoImage?.src} />
           </Text>
         </Flex>
 
-        <Stack flex={{ base: 1, md: 0 }} justify="flex-end" direction="row" spacing={6}>
+        <Stack
+          display={{ base: "none", md: "flex" }}
+          flex={{ base: 1, md: 0 }}
+          justify="flex-end"
+          direction="row"
+          spacing={6}
+        >
           <Button
             as="a"
             fontSize="20px"
@@ -77,30 +72,14 @@ export default function WithSubnavigation() {
       </Flex>
       <Collapse in={isOpen} animateOpacity>
         <Stack flex={{ base: 1, md: 0 }} justify="flex-end" direction="column" spacing={6}>
-          <Button
-            as="a"
-            fontSize="20px"
-            fontFamily="SfProRounded"
-            fontWeight={500}
-            color="#424852"
-            variant="link"
-            href="#"
-          >
+          <Button fontSize="20px" fontFamily="SfProRounded" fontWeight={500} color="#424852" variant="link" href="#">
             Зөвлөмж
           </Button>
-          <Button
-            as="a"
-            fontSize="20px"
-            fontFamily="SfProRounded"
-            fontWeight={500}
-            color="#424852"
-            variant="link"
-            href="#"
-          >
+          <Button fontSize="20px" fontFamily="SfProRounded" fontWeight={500} color="#424852" variant="link" href="#">
             Холбоо барих
           </Button>
           <Button
-            display={{ base: "none", md: "inline-flex" }}
+            alignSelf="center"
             borderRadius="17px"
             py="5px"
             px="20px"
